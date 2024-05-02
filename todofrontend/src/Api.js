af63ee9d8d55ccfbfdd729f2213f5a0364ba834e
santha kumar <santhakumar4343@gmail.com>
2024-05-02 13:17:17 +0530

    docker files are added

--- todofrontend/src/Api.js
+++ todofrontend/src/Api.js
@@ -1,3 +1,3 @@
 
-//  export const API_BASE_URL = 'http://13.233.111.56:8082';
-export const API_BASE_URL = 'http://localhost:8082';
+ export const API_BASE_URL = 'http://18.60.101.37:8082';
+// export const API_BASE_URL = 'http://localhost:8082';
